61194a9b68fdce4985533d5fc090853eb8583ac0
frank bottinga <parota.1996@gmail.com>
2022-12-10 04:16:27 +0100

    Implemented loadEntities and started on save

--- OracleFeeder/Feeder/Src/KeyStore.cs
+++ OracleFeeder/Feeder/Src/KeyStore.cs
@@ -1,10 +1,14 @@
-﻿using System.Security.Cryptography;
+﻿using log4net;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
 using System.Text;
+using TerraSharp.Key;
 
 namespace Feeder
 {
     public class KeyStore
     {
+        ILog logger = LogManager.GetLogger(typeof(KeyStore));
         public interface Entity
         {
             public string Name { get; set; }
@@ -26,6 +30,45 @@ namespace Feeder
             var encryption = ComputeEncryption(key, plainText);
             return Convert.ToHexString(encryption);
         }
+
+        public Entity[] loadEntities(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string json = reader.ReadToEnd();
+                   
+                        var entities = JsonConvert.DeserializeObject<Entity[]>(json);
+                        return entities is not null ? entities : new Entity[0];
+                }
+            }
+            catch(Exception ex) 
+            {
+                logger.Error("loadKeys", ex);
+                return new Entity[0];
+            }
+        }
+
+        public async void save(string filePath, string name, string password, string mnemonic) 
+        {
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+
+            var keys = loadEntities(filePath);
+
+            if (keys.Select(key => key.Name == name).Any())
+            {
+                throw new Exception("Key already exists by that name");
+            }
+
+            var mnemonicKey = new MnemonicKey(mnemonic, true);
+
+            var cipherText = encrypt(JsonConvert.SerializeObject(mnemonicKey), password);
+        }
+
         private byte[] ComputeEncryption(byte[] key, string plainText)
         {
             Aes aes = Aes.Create();
